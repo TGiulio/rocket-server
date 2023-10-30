fd90b57b21cb981382d8c0ff7f09f29cc3b6fa7a
Giulio Toldo <giulio.toldo@gmail.com>
2023-10-30 07:36:49 +0100

    add static page serving

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,41 @@
-fn main() {}
+#[macro_use]
+extern crate rocket;
+
+use rocket::fs::{relative, FileServer};
+
+#[get("/")]
+fn hello() -> &'static str {
+    "Hello, world!"
+}
+
+//this is run as main
+#[launch]
+fn rocket() -> _ {
+    rocket::build()
+        .mount("/", routes![hello])
+        .mount("/index", FileServer::from(relative!("static")))
+}
+
+#[cfg(test)]
+mod test {
+    use super::rocket;
+    use rocket::http::{ContentType, Status};
+    use rocket::local::blocking::Client;
+
+    #[test]
+    fn hello_test() {
+        let client = Client::tracked(rocket()).expect("valid rocket instance");
+        let response = client.get(uri!(super::hello)).dispatch();
+        assert_eq!(response.status(), Status::Ok);
+        assert_eq!(response.into_string().unwrap(), "Hello, world!");
+    }
+
+    #[test]
+    fn index_test() {
+        let client = Client::tracked(rocket()).expect("valid rocket instance");
+        let response = client.get(uri!("/index")).dispatch();
+        assert_eq!(response.status(), Status::Ok);
+        assert_eq!(response.content_type().unwrap(), ContentType::HTML);
+        // could use some other tests here but I don't know what to test
+    }
+}
