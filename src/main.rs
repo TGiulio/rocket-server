6bf2aab958dba5c9e134c2a477eabf861b1870d2
Giulio Toldo <giulio.toldo@gmail.com>
2023-11-17 07:24:27 +0100

    add post request with extractor

--- src/main.rs
+++ src/main.rs
@@ -2,18 +2,38 @@
 extern crate rocket;
 
 use rocket::fs::{relative, FileServer};
+use rocket::serde::json::Json;
+use rocket::serde::Deserialize;
+use std::str;
+
+#[derive(Deserialize)]
+struct Factors {
+    dividend: f32,
+    divisor: f32,
+}
 
 #[get("/")]
 fn hello() -> &'static str {
     "Hello, world!"
 }
 
+#[post("/divide", format = "json", data = "<factors>")]
+fn divide(factors: Json<Factors>) -> String {
+    if factors.divisor != 0. {
+        (factors.dividend / factors.divisor).to_string()
+    } else {
+        "cannot divide by 0".to_string()
+    }
+}
+
 //this is run as main
 #[launch]
 fn rocket() -> _ {
     rocket::build()
-        // route defined above
+        // routes defined above: get
         .mount("/", routes![hello])
+        // post
+        .mount("/", routes![divide])
         // route used to serve static files
         .mount("/index", FileServer::from(relative!("static")))
         .mount("/style", FileServer::from(relative!("static/css")))
@@ -41,5 +61,18 @@ mod test {
         assert_eq!(response.status(), Status::Ok);
         assert_eq!(response.content_type().unwrap(), ContentType::HTML);
         // could use some other tests here but I don't know what to test
+    }
+
+    #[test]
+    fn divide_test() {
+        let client = Client::tracked(rocket()).expect("valid rocket instance");
+        let response = client
+            .post(uri!("/divide"))
+            .header(ContentType::JSON)
+            .body(r#"{"dividend": 4, "divisor": 2}"#)
+            .dispatch();
+        assert_eq!(response.status(), Status::Ok);
+        assert_eq!(response.content_type().unwrap(), ContentType::Text);
+        assert_eq!(response.into_string().unwrap(), "2");
     }
 }
